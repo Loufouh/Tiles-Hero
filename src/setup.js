cffc80d558165dbab41debf535232b990087e289
Louis Masson <louis.masson@palissy.fr>
2019-06-30 16:25:59 +0200

    :construction: Fix alert keyboard mistake

--- src/setup.js
+++ src/setup.js
@@ -30,7 +30,7 @@ function setup() {
 		}
 	});
 
-	alert('To play with a key board, you can use "j, k, l, ;" keys');
+	alert('To play with a keyboard, you can use "j, k, l, ;" keys');
 
 	setInterval( () => tiles.push(new Tile(randomInt(0, 4), -width/2)), 1000/3);
 
